33598d0c645adff3e66130b2587551255f8531a7
ilyasemenov1 <sialogacc@gmail.com>
2021-11-12 17:02:12 +0300

    Weather_app - v1.3.1

--- web/js/main.js
+++ web/js/main.js
@@ -9,8 +9,11 @@ let add_button_center = document.querySelector("#add_button_center");
 let no_connection = document.querySelector(".no-connection-notification");
 let town_not_found = document.querySelector(".town-not-found-notification");
 let town_not_found_label = document.querySelector("#town_not_found_label");
+let enter_town_notification = document.querySelector(".enter-town-notification");
 let search_input = document.querySelector("#search_input");
+let latest_towns = document.querySelector(".latest-towns");
 var all_last_places_array = [];
+var last_places_array = [];
 let click_counter = 0;
 
 
@@ -21,40 +24,52 @@ async function display_weather(place) {
 
     if (is_online()) {
 
-        no_connection.className = "no-connection-notification no-connection-notification--disabled";
+        if (place) {
+            no_connection.className = "no-connection-notification no-connection-notification--disabled";
 
-        let search_animation = document.querySelector("#search_animation");
-        let weather_template = document.querySelector(".weather-template");
-    
-        search_animation.className = "search__animation-conteiner";
-        weather_template.style = "display: grid;";
-    
-        var main_data = await eel.main(place)();
-    
-        var weather_info_data = main_data[0];
-        let istrue = main_data[1];
-    
-        if (istrue) {
-    
-            var weather_data = document.querySelectorAll('.weather_data');
-    
-            let i = 0;
-            weather_info_data.forEach(element => {
-                weather_data[i].textContent = element;
-                i++;
-            });
-    
-            search_animation.className = "search__animation-conteiner  search__animation-conteiner--disactive";
-            weather_template.className = "weather-template weather-template--active";
-    
+            let search_animation = document.querySelector("#search_animation");
+            let weather_template = document.querySelector(".weather-template");
+        
+            search_animation.className = "search__animation-conteiner";
+            weather_template.style = "display: grid;";
+        
+            var main_data = await eel.main(place)();
+        
+            var weather_info_data = main_data[0];
+            let istrue = main_data[1];
+        
+            if (istrue) {
+
+                if (last_places_array[0] != place) {
+                    last_places_array.unshift(place);
+
+                    add_last_town(place);
+                }
+
+                var weather_data = document.querySelectorAll('.weather_data');
+        
+                let i = 0;
+                weather_info_data.forEach(element => {
+                    weather_data[i].textContent = element;
+                    i++;
+                });
+        
+                search_animation.className = "search__animation-conteiner  search__animation-conteiner--disactive";
+                weather_template.className = "weather-template weather-template--active";
+        
+            } else {
+                search_animation.className = "search__animation-conteiner  search__animation-conteiner--disactive";
+        
+                town_not_found_label.textContent = " \"" + place + "\" ";
+                town_not_found.className = "town-notification town-not-found-notification";
+            }
         } else {
-            search_animation.className = "search__animation-conteiner  search__animation-conteiner--disactive";
-    
-            town_not_found_label.textContent = " \"" + place + "\" ";
-            town_not_found.className = "town-not-found-notification";
+            print_weather_info(true);
+            enter_town_notification.className = "town-notification enter-town-notification";
         }
     } else {
-        no_connection.className = "no-connection-notification";
+        print_weather_info(true);
+        no_connection.className = "town-notification no-connection-notification";
     }
 }
 
@@ -70,8 +85,11 @@ function print_weather_info(par) {
         recommend_towns.style = "display: none;";
         weather_template.className = "weather-template";
         add_new_town.className = "add-new-town add-new-town--disactive";
-        town_not_found.className = "town-not-found-notification town-not-found-notification--disabled";
+        no_connection.className = "no-connection-notification no-connection-notification--disabled";
+        town_not_found.className = "town-notification town-not-found-notification town-not-found-notification--disabled";
+        enter_town_notification.className = "town-notification enter-town-notification enter-town-notification--disabled";
         click_counter = hide__add_new_town_button();
+        latest_towns.style = "display: none";
     } else {
         search.style = "";
         search_back_button.style = "";
@@ -84,6 +102,8 @@ function print_weather_info(par) {
         no_connection.className = "no-connection-notification no-connection-notification--disabled";
         town_not_found.className = "town-not-found-notification town-not-found-notification--disabled";
         town_not_found_label.textContent = "";
+        enter_town_notification.className = "town-notification enter-town-notification enter-town-notification--disabled";
+        latest_towns.style = "display: grid";
     }
 }
 
@@ -96,6 +116,7 @@ function document_events() {
     search_button.addEventListener('click', function() {
 
         let place = document.getElementById("search_input").value;
+
         display_weather(place);
     });
 
@@ -109,6 +130,16 @@ function document_events() {
 
     let search_input = document.querySelector("#search_input");
 
+    search_input.addEventListener('keydown', function(event) {
+        if (event.keyCode == 13) {
+            let place = document.getElementById("search_input").value;
+            display_weather(place);
+        }
+        if (event.keyCode == 27) {
+            print_weather_info(false);
+        }
+    });
+
     search_input.addEventListener('focus', function() {
         search_input_conteiner.style = "box-shadow: 0px 0px 18px #0000001f;";
     });
@@ -120,6 +151,8 @@ function document_events() {
     let refresh_button = document.querySelector("#refresh_button");
 
     refresh_button.addEventListener('click', function() {
+
+        print_weather_info(true);
 
         search_input.value = all_last_places_array[0];
 
@@ -138,14 +171,17 @@ function document_events() {
         print_weather_info(false);
     });
     
-    
     var town_close_button = document.querySelectorAll(".recommend-towns__close-button");
     var town_button = document.querySelectorAll(".recommend-towns__button");
     
     town_close_button_event(town_close_button, town_button);
+    town_button_event_focus(town_close_button, town_button, town_close_button, "focus");
+    town_button_event_focus(town_close_button, town_button, town_close_button, "blur");
 
     var town_name_button = document.querySelectorAll(".recommend-towns__town-button-name");
     town_name_button_event(town_name_button);
+    town_button_event_focus(town_name_button, town_button, town_close_button, "focus");
+    town_button_event_focus(town_name_button, town_button, town_close_button, "blur");
 
     let add_button = document.querySelector("#add_button");
 
@@ -155,9 +191,30 @@ function document_events() {
         } else {
             click_counter = hide__add_new_town_button();
         }
-
     });
 
+    add_button.addEventListener('keydown', function(event) {
+        if (event.keyCode == 27) {
+            click_counter = hide__add_new_town_button();
+        } 
+    });
+
+    let new_town_input = document.querySelector("#new_town_input");
+
+    let new_town_button = document.querySelector("#new_town_button");
+
+    new_town_button.addEventListener('click', function() {
+        new_town_button_event();
+    });
+
+    new_town_input.addEventListener('keydown', function(event) {
+        if (event.keyCode == 13) {
+            new_town_button_event();
+        }
+        if (event.keyCode == 27) {
+            click_counter = hide__add_new_town_button();
+        }
+    });
 }
 
 function show_add_new_town_button() {
@@ -182,30 +239,26 @@ function hide__add_new_town_button() {
     return 0;
 }
 
-function new_town_button_event() {
-
+async function new_town_button_event() {
     let new_town_button = document.querySelector("#new_town_button");
-
-    new_town_button.addEventListener('click', async function() {
-        if (new_town_button.id != "new_town_button_active") {
-            let new_town = document.getElementById("new_town_input").value;
-            if (new_town) {
-                new_town_button.className = "add-new-town__button add-new-town__button--active";
-                new_town_button.id = "new_town_button_active";
-                let main_data = await eel.is_town(new_town)();
-                if (main_data) {
-                    if (addendum_new_town(new_town)) {
-                        new_town_button.className = "add-new-town__button ";
-                        new_town_button.id = "new_town_button";
-                    }
-                } else {
+    if (new_town_button.id != "new_town_button_active") {
+        let new_town = document.getElementById("new_town_input").value;
+        if (new_town) {
+            new_town_button.className = "add-new-town__button add-new-town__button--active";
+            new_town_button.id = "new_town_button_active";
+            let main_data = await eel.is_town(new_town)();
+            if (main_data) {
+                if (addendum_new_town(new_town)) {
                     new_town_button.className = "add-new-town__button ";
                     new_town_button.id = "new_town_button";
                 }
+            } else {
+                new_town_button.className = "add-new-town__button ";
+                new_town_button.id = "new_town_button";
             }
         }
-    });
-}
+    }
+} 
 
  function addendum_new_town(town) {
 
@@ -237,21 +290,68 @@ function new_town_button_event() {
 
 }
 
-// TODO: Remake town_button_arr counter of the town_close_button_event function
+function add_last_town(town) {
+    let latest_towns_content = document.querySelector(".latest-towns__content-conteiner");
+
+    last_town_button = document.createElement("button");
+    last_town_button.className = "latest-towns__town-button";
+    last_town_button.textContent = town;
+    last_town_button.value = town;
+
+    let town_count = 0;
+    var last_town_buttons = document.querySelectorAll(".latest-towns__town-button");
+
+    if (last_town_buttons) {
+        town_count = last_town_buttons.length + 1;
+    }
+    
+    if (town_count > 4) {
+        latest_towns_content.prepend(last_town_button);
+        last_town_buttons = document.querySelectorAll(".latest-towns__town-button");
+        last_town_buttons[last_town_buttons.length - 1].remove();
+    } else {
+        latest_towns_content.prepend(last_town_button);
+        last_town_buttons = document.querySelectorAll(".latest-towns__town-button");
+    }
+
+    town_name_button_event([last_town_buttons[last_town_buttons.length - 1]]);
+}
+
+function town_button_event_focus(focus_button_arr, town_button_arr, close_button_arr, event) {
+    if (focus_button_arr && town_button_arr && close_button_arr) {
+        if (focus_button_arr.length == town_button_arr.length) {
+            if (event == "focus") {
+                for (let i = 0; i < focus_button_arr.length; i++) {
+                    focus_button_arr[i].addEventListener('focus', function() {
+                        town_button_arr[i].className = "recommend-towns__button recommend-towns__button--active";
+                        close_button_arr[i].style = "opacity: 1";
+                    });
+                }
+            }
+            else if (event == "blur") {
+                for (let i = 0; i < focus_button_arr.length; i++) {
+                    focus_button_arr[i].addEventListener('blur', function() {
+                        town_button_arr[i].className = "recommend-towns__button";
+                        close_button_arr[i].style = "";
+                    });
+                }
+            }
+        }
+    }
+}
+
 function town_close_button_event(close_button_arr, town_button_arr) {
-    if (close_button_arr.length >= 1 && town_button_arr.length >= 1) {
+    if (close_button_arr && town_button_arr) {
         if (close_button_arr.length == town_button_arr.length) {
-            let i = 0;
-            close_button_arr.forEach(element => {
-                element.addEventListener('click', function() {
+            for (let i = 0; i < close_button_arr.length; i++) {
+                close_button_arr[i].addEventListener('click', function() {
                     town_button_arr[i].remove();
                     var town_button_count = document.querySelectorAll(".recommend-towns__button");
                     if (town_button_count.length == 0) {
                         console.log("нет элементов");
                     }
-                    i++;
                 });
-            });
+            }
         }
     }
 }

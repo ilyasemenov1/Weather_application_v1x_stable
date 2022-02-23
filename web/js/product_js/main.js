7194fc1b174db6b445a6663c7552963540caa0b6
ilyasemenov1 <84145345+ilyasemenov1@users.noreply.github.com>
2022-02-23 16:42:17 +0300

    Weather_app - v1.3.6.3

--- web/js/product_js/main.js
+++ web/js/product_js/main.js
@@ -1,3 +1,25 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+module.exports={
+    "ui": {
+        "theme": {
+            "dark-theme": false
+        },
+        "latest-towns": {
+            "display-latest-towns": true
+        },
+        "recommend-towns": {
+            "display-recommend-towns": true,
+            "recommend-latest-towns": true,
+            "block-recommend-towns": false
+        },
+        "weather-info": {
+            "dasplay-weather-additional-info": true,
+            "dasplay-weather-forecast": true,
+            "dasplay-weather-forecast-icons": true
+        }
+    }
+}
+},{}],2:[function(require,module,exports){
 const json = require("../json/settings.json");
 
 document_events();
@@ -966,3 +988,4 @@ function is_online() {
 
     }
 }
+},{"../json/settings.json":1}]},{},[2]);
